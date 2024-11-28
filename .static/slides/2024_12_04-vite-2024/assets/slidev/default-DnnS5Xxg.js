0ce38c49697f0be45f43bc766f5f00b2e0936555
mnenie <121057011+mneniee@users.noreply.github.com>
2024-11-28 20:02:08 +0300

    chore: update ci/cd

--- .static/slides/2024_12_04-vite-2024/assets/slidev/default-DnnS5Xxg.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-D5j5L_-Y.js";import{_ as r}from"../index-CLVnuxME.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
--- .static/slides/2024_12_04-vite-2024/assets/vite-D_ZuIGw4.js
+++
@@ -1,1 +0,0 @@
-const s="/talks/slides/2024_12_04-vite-2024/base/vite.png";export{s as _};
