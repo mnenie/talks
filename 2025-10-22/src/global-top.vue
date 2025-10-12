ccaef1450f658190a8f7975128ee1cb178a88661
mnenie <1alexpeshkov@gmail.com>
2025-10-12 21:18:31 +0300

    feat: add global-top

---
+++ 2025-10-22/src/global-top.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useNav } from "@slidev/client";
+import { computed } from "vue";
+import GradientBackground from "./components/GradientBackground.vue";
+
+const { currentSlideRoute } = useNav();
+const frontmatter = computed(
+  () =>
+    (currentSlideRoute.value &&
+      currentSlideRoute.value.meta?.slide?.frontmatter) ||
+    {},
+);
+</script>
+
+<template>
+  <div :class="frontmatter.slide ?? 'slide-main'">
+    <div
+      :class="[
+        'absolute text-5xl transition-all duration-500',
+        frontmatter.topTitleClass,
+      ]"
+      :style="{ opacity: frontmatter.topTitle ? '70' : '0' }"
+    >
+      {{ frontmatter.topTitle }}
+    </div>
+  </div>
+</template>
